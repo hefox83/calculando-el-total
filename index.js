f56409c941d21f46a084c11a9c240c7e30959ac3
hefox83 <151103283+hefox83@users.noreply.github.com>
2024-03-02 01:58:01 -0300

    upload archvos

---
+++ index.js
@@ -0,0 +1,8 @@
+let precio = 400000;
+let numinicial = 1;
+let precioSpan = document.querySelector(".precio-inicial");
+let cantidad = document.querySelector (".cantidad");
+let preciofinal = document.querySelector(".valor-total");
+precioSpan.innerHTML = precio;
+cantidad.innerHTML = numinicial;
+total.innerHTM = precio-inicial;
